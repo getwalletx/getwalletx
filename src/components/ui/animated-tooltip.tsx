c7cdae4a0e7dcec5bb93aaaa9f1d5a24b5afb5cf
xhakti <117899415+xhakti@users.noreply.github.com>
2024-11-07 16:21:22 +0530

    Hero updating

--- src/components/ui/animated-tooltip.tsx
+++ src/components/ui/animated-tooltip.tsx
@@ -79,11 +79,11 @@ export const AnimatedTooltip = ({
           </AnimatePresence>
           <Image
             onMouseMove={handleMouseMove}
-            height={100}
-            width={100}
+            height={80}
+            width={80}
             src={item.image}
             alt={item.name}
-            className="object-cover !m-0 !p-0 object-top rounded-full h-10 w-10 sm:h-14 sm:w-14 border-2 group-hover:scale-105 group-hover:z-30 border-white relative transition duration-500"
+            className="object-cover !m-0 !p-0 object-top rounded-full h-10 w-10  border-2 group-hover:scale-105 group-hover:z-30 border-white relative transition duration-500"
           />
         </div>
       ))}
